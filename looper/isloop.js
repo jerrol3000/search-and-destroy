1a0e2425bfcca69dac2bdb7c86b22245ddfb176e
Jerral Graham <jerralgraham@yahoo.com>
2022-10-27 16:13:36 -0400

    isLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,12 +1,31 @@
 'use strict';
 
+const LinkedList = require("./linkedlist");
+
 //Complete this algo
 const isLoop = (linkedlist) => {
+  const cache = {}
+    function checkList(node){
+  // if cache[node.value]? return true
+    if(cache[node.value]) return true
 
+    cache[node.value] = true
+
+
+  if (node.next === null) return false
+
+      return checkList(node.next)
+    }
+   return checkList(linkedlist.head)
 };
 
+// const isLoop = (linkedlist)=>{
+//   if(!linkedlist.head) return false
+//   return linkedlist.tail.next !== null
 
+// }
 /*
+
 EXTRA CREDIT:
 
 Write a function findLoop() that consumes a linkedlist with a loop
